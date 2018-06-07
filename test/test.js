73b2333fb6860549f07396b0b7a00cbca80a6c44
Jonas Thiel <jt@neopoly.de>
2018-06-07 14:19:10 +0200

    Upgrade dependencies
    * Node 10
    * NPM 6.1
    * React 16
    * Babel 6
    * Introduct Enzyme

--- test/test.js
+++ test/test.js
@@ -1,1 +1,8 @@
+import Enzyme from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import chai from "chai"
+
+Enzyme.configure({ adapter: new Adapter() })
+
+// You can list more individual test files here:
 require("app_test")
